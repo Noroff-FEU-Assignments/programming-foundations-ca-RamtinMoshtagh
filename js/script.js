805eb78fb83135f6ca3fef3d3ac84d6f3c06ab6d
RamtinMoshtagh <95631899+RamtinMoshtagh@users.noreply.github.com>
2022-03-26 18:07:57 +0100

    created the ul and li
    harder than i expected

--- js/script.js
+++ js/script.js
@@ -10,3 +10,66 @@ else {
     console.log("In stock");
 }
 
+
+//question 2
+
+for (i=15; i >= 25; i++) {
+    if (i === 17 || i=== 20) {
+        console.log(i);
+    }
+}
+
+//question 3
+
+
+
+var games = [
+    {
+     title: "Grand Theft Auto",
+     rating: 4.48,
+    },
+    {
+     title: "Portal 2",
+     rating: 3.5,
+    },
+    {
+     title: "Team Fortress",
+     rating: null,
+    },
+    {
+     title: "The Witcher",
+     rating: 3.0,
+    },
+    {
+     title: "The Elder Scrolls",
+     rating: 2.9,
+    },
+   ];
+
+
+   
+/*    games.forEach(function(game) {
+       if(game.rating && game.rating < 3.5){
+const li = document.createElement("li");
+            li.appendChild(document.createTextNode(`${game.title}  ${game.rating}`));
+            ul.appendChild(li);
+        }
+   }) */
+
+
+   var ul = document.querySelector("#games");
+   console.log(ul);
+   
+
+   for (var i=0; i < games.length; i++) {  
+        if (games[i].rating < 3.5 && games[i].rating) {
+        
+            // lage en li
+            const li = document.createElement("li");
+            // lage en TextNode med navn på spill og rating.
+            li.appendChild(document.createTextNode (games[i].title));
+            li.appendChild(document.createTextNode (games[i].rating))
+            // også må jeg hekte den på ul.
+            ul.appendChild(li);
+       }
+   }
